906efc0f4eef0d96e5949eb53b8e76e56faab5cd
mineru98 <rmstjr1030@naver.com>
2020-02-09 14:30:17 +0000

    Update Code

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import {default as classnames} from 'classnames'
+// import * as classnames from 'classnames'
+import * as css from './Home.css'
+
+export const Home: React.FunctionComponent = props =>
+  <div className={classnames('test', css.home)}>
+    <ul>
+      <li>
+        usage classnames in Home.tsx
+      </li>
+      <li>
+        Layout.tsx set background-color hot-pink using global styled jsx
+      </li>
+    </ul>
+  </div>
---
+++ src/redux/router/index.ts
@@ -0,0 +1,11 @@
+import Router from 'next/router';
+
+export function addRouteCompleteEvent(fx: (url) => void) {
+    onRouteCompleteEvents.push(fx);
+}
+
+const onRouteCompleteEvents = [];
+
+Router.events.on('routeChangeComplete', url => {
+    onRouteCompleteEvents.forEach(fx => fx(url));
+});
